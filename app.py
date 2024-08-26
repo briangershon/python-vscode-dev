4511462ab73135c6b6fd59d76c900546ae36775d
Brian Gershon <briangershon@users.noreply.github.com>
2024-08-26 16:01:25 -0700

    Add example LangChain and OpenAI app. (#2)
    Plus fix `.env.example` -- shouldn't have quotes

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-def greeting(name: str) -> str:
-    return f"Hello, {name}!"
+from langchain.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
 
+llm = ChatOpenAI(temperature=0.9, model="gpt-4o-mini")
+prompt = ChatPromptTemplate.from_template("Create a haiku from:\n\n{content}")
+chain = prompt | llm
 
-print(greeting("World"))
+response = chain.invoke({"content": "The quick brown fox jumps over the lazy dog."})
+print(response.content)
